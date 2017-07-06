c5e8d253cabfbefc96070e0ac9c445c5643d7b74
raigomes <rai.gomes.ismart@gmail.com>
2017-07-06 01:57:13 -0300

    Implementando lógica de preenchimento do client de email com JavaScript

---
+++ js/main.js
@@ -0,0 +1,62 @@
+$(document).ready(function() {
+	carregaImagensLocais();
+	//carregaPortfolio();
+});
+
+String.prototype.replaceAll = function(search, replacement) {
+    var target = this;
+    return target.replace(new RegExp(search, 'g'), replacement);
+};
+
+function carregaImagensLocais() {	
+	$("#sobre-foto img")
+		.one('load', function() { console.log("Imagens carregadas com sucesso"); })
+		.one('error', function() { console.log("Deu ruim"); })
+		.each(function() {if(this.complete) $(this).load()});
+	;
+}
+
+function carregaEmail() {	
+	var answer = document.querySelectorAll(".form");	
+	var url = "mailto:raigomes2 at hotmail dot com";		
+
+	if(validaFormulario(answer)) {
+		var senderName = answer[0].value;
+		var senderEmail = answer[1].value;
+		var senderPhone = answer[2].value;
+		var senderMessage = answer[3].value;
+
+		var subject = senderName + " deixou uma mensagem no seu site";
+		var body = "[Nome: " + senderName + " Email: " + senderEmail + " Telefone: " + senderPhone + "]" + "\r\n\r\n" + senderMessage;
+
+		url = normalizaEmail(url) + "?subject=" + substituiCaracteresEspeciais(subject) + "&body=" + substituiCaracteresEspeciais(body);
+
+		console.log(url);
+		window.open(url);
+
+	}
+	else {
+		alert("Preencha os campos corretamente");
+	}
+	
+}
+
+function validaFormulario(vector) {
+	if(vector == null) return false;
+
+	for(var i = 0; i < vector.length - 2; i++) {
+		if(vector[i].value == "") return false;
+	}
+
+	return true;
+}
+
+function normalizaEmail(email) {
+	return email.replaceAll(" at ", "@")
+				.replaceAll(" dot ", ".")				
+	;
+}
+
+function substituiCaracteresEspeciais(text) {
+	return encodeURIComponent(text);
+}

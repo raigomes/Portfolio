437fd734b69df0bb9608b6460d83282adf93e1ad
raigomes <rai.gomes.ismart@gmail.com>
2017-07-11 02:44:37 -0300

    Iniciando a criação do carregamento dos trabalhos do portfolio dinamicamente via JSON e Ajax

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	//carregaImagensLocais();
-	//carregaPortfolio();
+	carregaPortfolio();
 });
 
 String.prototype.replaceAll = function(search, replacement) {
@@ -8,64 +8,59 @@ String.prototype.replaceAll = function(search, replacement) {
     return target.replace(new RegExp(search, 'g'), replacement);
 };
 
-function carregaImagensLocais() {	
+/*function carregaImagensLocais() {	
 	$("#sobre-foto img")
 		.one('load', function() { console.log("Imagens carregadas com sucesso"); })
 		.one('error', function() { console.log("Deu ruim"); })
 		.each(function() {if(this.complete) $(this).load()});
 	;
-}
+}*/
 
-function carregaEmail() {	
-	var answer = document.querySelectorAll(".form");	
-	var url = "mailto:raigomes2 at hotmail dot com";		
+function carregaPortfolio() {
+	var portfolio = document.getElementById("portfolio");
+	var newPort = buscaNovoPortfolio();
 
-	if(validaFormulario(answer)) {
-		var senderName = answer[0].value;
-		var senderEmail = answer[1].value;
-		var senderPhone = answer[2].value;
-		var senderMessage = answer[3].value;
-
-		var subject = senderName + " deixou uma mensagem no seu site";
-		var body = "[Nome: " + senderName + " Email: " + senderEmail + " Telefone: " + senderPhone + "]" + "\r\n\r\n" + senderMessage;
-
-		url = normalizaEmail(url) + "?subject=" + substituiCaracteresEspeciais(subject) + "&body=" + substituiCaracteresEspeciais(body);
-
-		console.log(url);
-		window.open(url);
-
-		limpaCamposForm(answer);
-
-		alert("Dados enviados para seu cliente de email");
+	if(newPort != "") {
+		portfolio.innerHTML = newPort;
 	}
-	else {
-		alert("Preencha os campos corretamente");
-	}	
-	
 }
 
-function validaFormulario(vector) {
-	if(vector == null) return false;
+function buscaNovoPortfolio() {
+	var jobs = buscaNovoPortfolioRequest();
 
-	for(var i = 0; i < vector.length - 2; i++) {
-		if(vector[i].value == "") return false;
+	if(jobs === null || jobs.length == 0) {
+		return "";
 	}
+	else{
+		var count = 1;
 
-	return true;
-}
-
-function normalizaEmail(email) {
-	return email.replaceAll(" at ", "@")
-				.replaceAll(" dot ", ".")				
-	;
-}
-
-function substituiCaracteresEspeciais(text) {
-	return encodeURIComponent(text);
-}
-
-function limpaCamposForm(campos) {
-	for (var i = campos.length - 2; i >= 0; i--) {
-		campos[i].value = '';
+		/*return `<h1>Portfolio</h1><hr>
+				<div class="container">
+					<div class="row">
+						${jobs.map(job => ${makeJobDiv(jobs, count++)})}
+					</div>
+				</div>
+				`;
+		S*/
 	}
+}
+
+function makeJobDiv(jobs, id) {
+	return `<div id="port${id}" class="col-xs-12 col-sm-6 col-md-3">
+					<div class="thumbnail"><a href=${jobs.url}>
+						<figure class="figure">
+							<img src=${jobs.imageUrl} alt="" class="figure-img img-fluid port-img" id="port-img-1">
+							<figcaption class="figure-caption port-caption">${jobs.titulo}</figcaption>
+						</figure>
+					</a></div>
+				</div>`;
+}
+
+function buscaNovoPortfolioRequest() {
+	return [`{  
+         "id": 1,
+         "titulo": "Tribute Page",
+         "url": "https://codepen.io/raigomes/pen/BpVpxr",
+         "imageUrl": "https://c2.staticflickr.com/4/3689/10613180113_fdf7bcd316_b.jpg"
+      }`];
 }
